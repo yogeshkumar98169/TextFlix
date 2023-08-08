c747731b35cd06e89e25cbaf8dd2c23d0900645a
Yogesh Kumar <yogeshkumar.pc00@gmail.com>
2023-08-08 18:22:52 +0530

    Removed module1.mjs and module2.mjs

--- src/module1.mjs
+++
@@ -1,5 +0,0 @@
-import dz, { a, b, c } from "./module2.mjs";
-console.log(dz);
-console.log(a);
-console.log(b);
-console.log(c);
--- src/module2.mjs
+++
@@ -1,6 +0,0 @@
-const a = "Yogesh";
-const b = "Aakash";
-const c = "Aman";
-const d = "Rishu";
-export default d;
-export { a, b, c };
