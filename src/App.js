7cd3d525cf5fc77a8005594e11611493e997feb6
Yogesh Kumar <yogeshkumar.pc00@gmail.com>
2023-08-09 17:22:32 +0530

    Added functionalities + dark mode progress

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import { useState } from "react";
 import "./App.css";
 import About from "./components/About";
 import Navbar from "./components/Navbar.js";
 import TextForm from "./components/TextForm";
 function App() {
+  const [mode, setMode] = useState('light')  //!Whether dark mode is enabled or not
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About TextUtils" />
+      <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} />
       <TextForm heading="Enter the Text"></TextForm>
-      {/* <About></About> */}
+      <About></About>
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
       <a className="navbar-brand" href="/">
         {props.title}
       </a>
